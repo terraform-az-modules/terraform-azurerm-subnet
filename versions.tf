1e4153fbe1e624798447bcc2d34fd78d680ee230
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 15:54:32 -0400

    chore: polish module with basic example, changelog, and version fixes

--- versions.tf
+++ versions.tf
@@ -1,18 +1,12 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
 terraform {
   required_version = ">= 1.10.0"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">=3.116.0"
     }
   }
-
   provider_meta "azurerm" {
     module_name = "terraform-az-modules/terraform-azurerm-subnet"
   }
